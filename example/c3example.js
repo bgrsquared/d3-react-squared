1b9c14ebaac84bebed4a881d8a452fa5439d2654
chroth7 <chroth@me.com>
2015-09-07 09:37:42 +0200

    Add focus and revert api calls on c3.

--- example/c3example.js
+++ example/c3example.js
@@ -38,11 +38,27 @@ export default class C3Example extends Component {
     this.setState({c3obj});
   }
 
+  focusData(id) {
+    let c3obj = {c3arg: id, c3fct: 'focus'};
+    this.setState({c3obj});
+  }
+
+  revert(id) {
+    let c3obj = {c3arg: id, c3fct: 'revert'};
+    this.setState({c3obj});
+  }
+
   render() {
     return (
       <div>
         <button onClick={this.newData.bind(this)}>
           Flip lines
+        </button>
+        <button onClick={this.focusData.bind(this, 'data1')}>
+          Focus 1
+        </button>
+        <button onClick={this.revert.bind(this, '')}>
+          Revert all
         </button>
         <DR2 c3obj={this.state.c3obj}/>
       </div>
