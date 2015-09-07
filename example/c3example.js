4f4c7705f245671a806c87bcf032060cc4d52871
chroth7 <chroth@me.com>
2015-09-07 17:41:48 +0200

    Clean up c3exmpale page.

--- example/c3example.js
+++ example/c3example.js
@@ -8,65 +8,118 @@ export default class C3Example extends Component {
   constructor() {
     super();
     this.state = {
-      c3obj: {
-        c3arg: {
-          bindto: '#hui',
-          data: {
-            columns: [
-              ['data1', 30, 200, 100, 400, 150, 250],
-              ['data2', 50, 20, 10, 40, 15, 25],
-              ['data3', -50, -20, -10, -40, -15, -25]
-            ]
-          },
-          legend: {
-            item: {
-              onmouseover: function(id) {
-                let eventObj = {
-                  data: {id}, event: 'mouseover', eventGroup: ['default']
-                };
-                this.api.setEvent(eventObj);
-              },
-              onmouseout: function(id) {
-                let eventObj = {
-                  data: {id}, event: 'mouseout', eventGroup: ['default']
-                };
-                this.api.setEvent(eventObj);
-              }
+      c3obj1: false,
+      c3obj2: false
+    }
+  }
 
-            }
-          }
+  myRand() {
+    return Math.floor(100 * Math.random());
+  }
+
+  myRandVector(id) {
+    let vect = Array.from({length: 6}, () => this.myRand());
+    vect.unshift('data' + id);
+    return vect;
+  }
+
+  fakeC3LineData() {
+    let data = [];
+    for (let i = 0; i < 3; i++) {
+      data.push(this.myRandVector(i + 1));
+    }
+    return data;
+  }
+
+  dr2legend() {
+    return {
+      item: {
+        onmouseover: function(id) {
+          let eventObj = {
+            data: {id}, event: 'mouseover', eventGroup: ['default']
+          };
+          this.api.setEvent(eventObj);
         },
-        c3fct: 'generate'
+        onmouseout: function(id) {
+          let eventObj = {
+            data: {id}, event: 'mouseout', eventGroup: ['default']
+          };
+          this.api.setEvent(eventObj);
+        }
       }
     }
   }
 
-  newData() {
+  c3chart1gen() {
+    this.fakeC3LineData();
+    return {
+      c3arg: {
+        bindto: '#chart1',
+        data: {
+          columns: this.fakeC3LineData(),
+          groups: [['data1', 'data2', 'data3']],
+          type: 'area'
+        },
+        legend: this.dr2legend()
+      },
+      c3fct: 'generate'
+    }
+  }
+
+  c3chart2gen() {
+    return {
+      c3arg: {
+        bindto: '#chart2',
+        data: {
+          columns: this.fakeC3LineData(),
+          type: 'spline'
+        },
+        legend: this.dr2legend()
+      },
+      c3fct: 'generate'
+    }
+  }
+
+
+  newData(obj) {
     let c3obj = {
       c3arg: {
-        columns: [
-          ['data1', -30, -200, -100, -400, -150, -250],
-          ['data2', -50, -20, -10, -40, -15, -25],
-          ['data3', 50, 20, 10, 40, 15, 25]
-        ]
+        columns: this.fakeC3LineData()
       },
       c3fct: 'load'
     };
-    this.setState({c3obj});
+    this.setState({[obj]: c3obj});
   }
 
-  focusData(id) {
+  focusData(id, obj) {
     let c3obj = {c3arg: id, c3fct: 'focus'};
-    this.setState({c3obj});
+    this.setState({[obj]: c3obj});
   }
 
-  revert(id) {
+  revert(id, obj) {
     let c3obj = {c3arg: id, c3fct: 'revert'};
-    this.setState({c3obj});
+    this.setState({[obj]: c3obj});
+  }
+
+  groupLines(obj) {
+    let c3obj = {
+      c3arg: [['data1', 'data2', 'data3']],
+      c3fct: 'groups'
+    };
+    this.setState({[obj]: c3obj});
+  }
+
+  ungroupLines(obj) {
+    let c3obj = {
+      c3arg: [[]],
+      c3fct: 'groups'
+    };
+    this.setState({[obj]: c3obj});
   }
 
   render() {
-    let fakeData = () => {
+    let {c3obj1, c3obj2} = this.state;
+    let fakeDataBar = () => {
       return [
         {id: 'data1', value: 1 + Math.floor(10 * Math.random())},
         {id: 'data2', value: 1 + Math.floor(10 * Math.random())},
@@ -74,21 +127,50 @@ export default class C3Example extends Component {
       ];
     };
 
+    if (!c3obj1) {
+      c3obj1 = this.c3chart1gen();
+    }
+    if (!c3obj2) {
+      c3obj2 = this.c3chart2gen();
+    }
+
     return (
       <div>
-        <button onClick={this.newData.bind(this)}>
-          Flip lines
-        </button>
-        <button onClick={this.focusData.bind(this, 'data1')}>
-          Focus 1
-        </button>
-        <button onClick={this.revert.bind(this, '')}>
-          Revert all
-        </button>
-        <DR2 c3obj={this.state.c3obj}/>
+        <div style={{display: 'flex'}}>
+          <div style={{width: '50%'}}>
+            <button onClick={this.newData.bind(this, 'c3obj1')}>
+              New Data
+            </button>
+            <button onClick={this.focusData.bind(this, 'data1', 'c3obj1')}>
+              Focus 1
+            </button>
+            <button onClick={this.revert.bind(this, '', 'c3obj1')}>
+              Revert all
+            </button>
+            <button onClick={this.groupLines.bind(this, 'c3obj1')}>
+              Group all
+            </button>
+            <button onClick={this.ungroupLines.bind(this, 'c3obj1')}>
+              Ungroup all
+            </button>
+            <DR2 c3obj={c3obj1}/>
+          </div>
+          <div style={{width: '50%'}}>
+            <button onClick={this.newData.bind(this, 'c3obj2')}>
+              New Data
+            </button>
+            <button onClick={this.focusData.bind(this, 'data1', 'c3obj2')}>
+              Focus 1
+            </button>
+            <button onClick={this.revert.bind(this, '', 'c3obj3')}>
+              Revert all
+            </button>
+            <DR2 c3obj={c3obj2}/>
+          </div>
+        </div>
         <div>
           <DR2
-            data={fakeData()}
+            data={fakeDataBar()}
           />
         </div>
       </div>
